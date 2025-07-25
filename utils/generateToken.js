80d3719c3296ae35e0db884b82ba5d3dbbd9c566
Rana Islam <dev.rana.cse@gmail.com>
2025-07-25 17:16:20 +0600

    generateToken.js update

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -9,7 +9,7 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    sameSite: 'none', // Prevent CSRF attacks and allow cross-site cookies
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
